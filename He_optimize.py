fa3bd68b9b58acb106bdddc35b57d7fee72e1d15
Wei Chuan Wu <weichuw5@uci.edu>
2025-09-18 08:43:20 -0700

    change atomic scf to src

--- He_optimize.py
+++ He_optimize.py
@@ -1,6 +1,6 @@
 # main.py — Helium (He) with s-only exponent optimization + SCF
 import numpy as np
-from atomic_scf import (
+from src import (
     make_basis, scf_loop,
     even_temper, optimize_zeta_minimal
 )
